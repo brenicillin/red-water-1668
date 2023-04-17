c04ede161d81a04dceed8cc5d0b2ad41348555e3
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 15:50:31 -0400

    add helper method for test data

---
+++ spec/helper_methods.rb
@@ -0,0 +1,13 @@
+def test_data
+  @brandon = Chef.create!(name: "Brandon Johnson")
+  @buffalo_mac = @brandon.dishes.create!(name: "Buffalo Mac and Cheese", description: "Chicken Breast tossed in Buffalo Sauce, smothered in cheese with macaroni noodles")
+  @chicken = Ingredient.create!(name: "Chicken Breast", calories: 200)
+  @mac = Ingredient.create!(name: "Macaroni Noodles", calories: 200)
+  @cheese = Ingredient.create!(name: "Cheese", calories: 500)
+  @buffalo = Ingredient.create!(name: "Buffalo Sauce", calories: 100)
+  @buffalo_mac.ingredients << @chicken
+  @buffalo_mac.ingredients << @buffalo
+  @buffalo_mac.ingredients << @mac
+  @buffalo_mac.ingredients << @cheese
+  require 'pry'; binding.pry
+end
