7ad9af8da704df730abc0ffc300fba52274cc3e6
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 16:47:16 -0400

    add chicken sando to ensure ingredients are unique

--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -4,7 +4,8 @@ def test_data
   
   @guy_burger = @guy.dishes.create!(name: "Guy Burger", description: "Burger with Guy's secret sauce")
   @buffalo_mac = @brandon.dishes.create!(name: "Buffalo Mac and Cheese", description: "Chicken Breast tossed in Buffalo Sauce, smothered in cheese with macaroni noodles")
-  
+  @chicken_sando = @guy.dishes.create!(name: "Chicken Sandwich", description: "Chicken Breast with Guy's secret sauce")
+
   @beef = Ingredient.create!(name: "Ground Beef Patty", calories: 500)
   @bun = Ingredient.create!(name: "Bun", calories: 200)
   @guy_sauce = Ingredient.create!(name: "Guy Sauce", calories: 100)
@@ -22,6 +23,11 @@ def test_data
   @guy_burger.ingredients << @beef
   @guy_burger.ingredients << @lettuce
   @guy_burger.ingredients << @guy_sauce
+
+  @chicken_sando.ingredients << @chicken
+  @chicken_sando.ingredients << @bun
+  @chicken_sando.ingredients << @guy_sauce
+  @chicken_sando.ingredients << @lettuce
 
   @buffalo_mac.ingredients << @chicken
   @buffalo_mac.ingredients << @buffalo
