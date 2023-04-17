e62064a45f0d18ddced113f04e4395a986d983ca
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 16:10:55 -0400

    add add_ingredient action

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,11 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def add_ingredient
+    dish = Dish.find(params[:id])
+    ingredient = Ingredient.find(params[:ingredient_id])
+    dish.ingredients << ingredient
+    redirect_to "/dishes/#{dish.id}"
+  end
 end
