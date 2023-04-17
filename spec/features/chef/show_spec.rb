22d622c77a7f4b20dcc61940dcc1382003f75800
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 17:04:32 -0400

    add extension show page tests

--- spec/features/chef/show_spec.rb
+++ spec/features/chef/show_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Chef Show Page' do
       expect(page).to have_content("Macaroni Noodles")
       expect(page).to have_content("Cheese")
       expect(page).to have_content("Buffalo Sauce")
-      expect(page).to_not have_content("Ground Beef")
+      expect(page).to have_content("Ground Beef")
       expect(page).to_not have_content("Bun")
       expect(page).to_not have_content("Tomato")
       expect(page).to_not have_content("Lettuce")
@@ -48,6 +48,28 @@ RSpec.describe 'Chef Show Page' do
       expect(page).to_not have_content("Macaroni Noodles")
       expect(page).to_not have_content("Cheese")
       expect(page).to_not have_content("Buffalo Sauce")
+    end
+  end
+
+  describe 'Extension' do
+    it 'shows the three most popular ingredients' do
+      visit "/chefs/#{@guy.id}"
+
+      within "#top_3_ingredients" do
+        expect(page).to have_content("Top 3 Most Popular Ingredients")
+        expect(page).to have_content(@bun.name)
+        expect(page).to have_content(@lettuce.name)
+        expect(page).to have_content(@guy_sauce.name)
+      end
+
+      visit "/chefs/#{@brandon.id}"
+
+      within "#top_3_ingredients" do
+        expect(page).to have_content("Top 3 Most Popular Ingredients")
+        expect(page).to have_content(@beef.name)
+        expect(page).to have_content(@mac.name)
+        expect(page).to have_content(@cheese.name)
+      end
     end
   end
 end
