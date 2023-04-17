976a5b0b1f77b70396d51bf3b3e81363650cedc5
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 15:36:48 -0400

    create dish show page spec

---
+++ spec/features/dish/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish Show Page' do
+  describe 'User Story 1' do
+    it "can see the dish's name and description" do
+      visit '/dishes/1'
+
+      within "#dish_info" do
+        expect(page).to have_content("Buffalo Mac and Cheese")
+        expect(page).to have_content("Chicken Breast tossed in Buffalo Sauce, smothered in cheese with macaroni noodles")
+        expect(page).to have_content("Total Calories: 1000")
+      end
+
+      within "#dish_ingredients" do
+        expect(page).to have_content("Chicken Breast")
+        expect(page).to have_content("Macaroni Noodles")
+        expect(page).to have_content("Cheese")
+      end
+
+      within "#prepared_by" do
+        expect(page).to have_content("Chef: Brandon Johnson")
+      end
+    end
+  end
+end
