d0122796969db1f9994674eeae841a417a1abfd4
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 16:31:25 -0400

    instance method for unique ingredients

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -6,5 +6,16 @@ RSpec.describe Chef, type: :model do
   end
   describe "relationships" do
     it {should have_many :dishes}
+    it {should have_many(:ingredients).through(:dishes)}
+  end
+
+  describe 'instance methods' do
+    before(:all) do
+      test_data
+    end
+
+    it 'unique_ingredients' do
+      expect(@brandon.unique_ingredients.sort).to eq([@mac, @cheese, @buffalo, @chicken].sort)
+    end
   end
 end
