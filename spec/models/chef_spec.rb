c752350fe8a69b63c1ae5aac71c22ef0c95d3cd7
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 17:03:37 -0400

    add top_3_ingredients test

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -15,7 +15,12 @@ RSpec.describe Chef, type: :model do
     end
 
     it 'unique_ingredients' do
-      expect(@brandon.unique_ingredients.sort).to eq([@mac, @cheese, @buffalo, @chicken].sort)
+      expect(@brandon.unique_ingredients.sort).to eq([@mac, @cheese, @buffalo, @chicken, @beef].sort)
+    end
+
+    it 'top_3_ingredients' do
+      expect(@guy.top_3_ingredients).to eq([@bun, @guy_sauce, @lettuce])
+      expect(@brandon.top_3_ingredients).to match_array([@beef, @mac, @cheese])
     end
   end
 end
