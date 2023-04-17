1c62fe641e03a8ef8ab4606d8d6f23a613dc00ee
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 16:04:38 -0400

    add extra ingredient

--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -5,6 +5,7 @@ def test_data
   @mac = Ingredient.create!(name: "Macaroni Noodles", calories: 200)
   @cheese = Ingredient.create!(name: "Cheese", calories: 500)
   @buffalo = Ingredient.create!(name: "Buffalo Sauce", calories: 100)
+  @sriracha = Ingredient.create!(name: "Sriracha", calories: 50)
   @buffalo_mac.ingredients << @chicken
   @buffalo_mac.ingredients << @buffalo
   @buffalo_mac.ingredients << @mac
