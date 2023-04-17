c54ced7f4c91728ea064b0a1ed8d535e7a43eab5
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-17 16:19:53 -0400

    update routes for US3;

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :dishes, only: [:show]
   post '/dishes/:id', to: 'dishes#add_ingredient'
 
+  resources :chefs, only: [:show] do
+    resources :ingredients, only: [:index]
+  end
+
 end
